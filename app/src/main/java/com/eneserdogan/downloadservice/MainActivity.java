aed30ce8bb5d1ded2dfc7ede88ecde699f4c6e36
eneserdo1 <eneserdogan.66@outlook.com>
2020-04-23 01:53:20 +0300

    new

---
+++ app/src/main/java/com/eneserdogan/downloadservice/MainActivity.java
@@ -0,0 +1,27 @@
+package com.eneserdogan.downloadservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void download(View view){
+        Intent intent=new Intent(this,downloadService.class);
+        startService(intent);
+    }
+
+    public void stop(View view){
+        Intent intent=new Intent(this,downloadService.class);
+        stopService(intent);
+
+    }
+}
